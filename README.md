c7ff26c376b0aaf4542e7b57bce9f1005f08d3cb
Leo Wong <leoelicos@gmail.com>
2022-03-02 22:06:06 +1100

    Create index, readme, script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# bcs-03-rock-paper-scissors
- 
+# Rock, Paper, Scissors
+
+In this activity I built a game of Rock, Paper, Scissors in Javascript. I wrote this program to learn about Array methods and also the alert(), confirm() and prompt() methods in Javascript. It helped me understand what a function is and how useful it can be.
+
+## Usage
+
+The game can be played by opening [Rock Paper, Scissors](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert) in Google Chrome.
+
+## Licence
+
+MIT Licence © 2022 Leo Wong
---
+++ assets/scripts/main.js
@@ -0,0 +1,55 @@
+// rock, paper, scissors
+
+const rock = 'R';
+const paper = 'P';
+const scissors = 'S';
+
+// tallies the player's wins
+var player = {
+	wins: 0,
+	ties: 0,
+	losses: 0,
+};
+
+// 0 = rock, 1 = paper, 2 = scissors
+var gameChoices = [rock, paper, scissors];
+
+window.alert('Rock-Paper-Scissors\r\nRules:\r\nrock (R) beats scissors (S)\r\npaper (P) beats rock (R)\r\nscissors (S) beats paper (P)');
+
+var keepPlaying = window.confirm('Play ?');
+
+while (keepPlaying === true) {
+	var playerChoice = window.prompt('To play, type R, P or S', 'R');
+
+	if (playerChoice == null) {
+		// player has selected cancel
+		keepPlaying = false;
+	} else {
+		playerChoice = playerChoice.toUpperCase();
+
+		var computerChoice = gameChoices[random(0, 2)];
+
+		if (playerChoice === computerChoice) {
+			player.ties++;
+		} else if ((playerChoice === rock && computerChoice === scissors) || (playerChoice === scissors && computerChoice === paper) || (playerChoice === paper && computerChoice === rock)) {
+			player.wins++;
+		} else {
+			player.losses++;
+		}
+
+		keepPlaying = window.confirm('You have ' + player.wins + ' wins, ' + player.ties + ' ties, ' + player.losses + ' losses.\r\nPlay again?');
+	}
+}
+
+window.alert('Thanks for playing, bye now!');
+
+/*********** 
+  Functions
+ ***********/
+
+// This function generates an integer from min to max
+// copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+function random(min, max) {
+	const num = Math.floor(Math.random() * (max - min + 1)) + min;
+	return num;
+}

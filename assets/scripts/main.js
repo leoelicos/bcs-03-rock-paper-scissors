04c5f43f1d2b33fd15f911f0c15a2f764e46b32e
Leo Wong <leoelicos@gmail.com>
2022-03-03 13:01:49 +1100

    added var result, removed function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,42 +14,65 @@ var player = {
 // 0 = rock, 1 = paper, 2 = scissors
 var gameChoices = [rock, paper, scissors];
 
-window.alert('Rock-Paper-Scissors\r\nRules:\r\nrock (R) beats scissors (S)\r\npaper (P) beats rock (R)\r\nscissors (S) beats paper (P)');
+window.alert('*********************\nRock-Paper-Scissors\n*********************\n\nRules:\nrock (R) beats scissors (S)\npaper (P) beats rock (R)\nscissors (S) beats paper (P)');
 
 var keepPlaying = window.confirm('Play ?');
 
 while (keepPlaying === true) {
 	var playerChoice = window.prompt('To play, type R, P or S', 'R');
+	var result = 'This game: ';
 
+	// player selects cancel
 	if (playerChoice == null) {
-		// player has selected cancel
 		keepPlaying = false;
-	} else {
+	}
+	// player wants to play
+	else {
 		playerChoice = playerChoice.toUpperCase();
 
-		var computerChoice = gameChoices[random(0, 2)];
+		result += 'You chose ';
+		if (playerChoice === rock) {
+			result += 'rock';
+		} else if (playerChoice === paper) {
+			result += 'paper';
+		} else if (playerChoice === scissors) {
+			result += 'scissors';
+		}
+		result += '.\n';
 
+		var computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+
+		result += 'Computer chose ';
+		if (computerChoice === rock) {
+			result += 'rock';
+		} else if (computerChoice === paper) {
+			result += 'paper';
+		} else if (computerChoice === scissors) {
+			result += 'scissors';
+		}
+		result += '.\n';
+
+		// tie
 		if (playerChoice === computerChoice) {
 			player.ties++;
-		} else if ((playerChoice === rock && computerChoice === scissors) || (playerChoice === scissors && computerChoice === paper) || (playerChoice === paper && computerChoice === rock)) {
-			player.wins++;
-		} else {
-			player.losses++;
+			result += 'Result = Tie.\n';
 		}
 
-		keepPlaying = window.confirm('You have\n' + player.wins + ' wins,\n' + player.ties + ' ties,\n' + player.losses + ' losses.\n\nPlay again?');
+		// player wins
+		else if ((playerChoice === rock && computerChoice === scissors) || (playerChoice === scissors && computerChoice === paper) || (playerChoice === paper && computerChoice === rock)) {
+			player.wins++;
+			result += 'Result = You win!\n';
+		}
+
+		// player loses
+		else {
+			player.losses++;
+			result += 'Result = You lose!\n';
+		}
+
+		// display stats and ask if they wanna play again
+		keepPlaying = window.confirm(result + '\nYour current standings:\n' + player.wins + ' wins, ' + player.ties + ' ties, ' + player.losses + ' losses.\n\nPlay another game?');
 	}
 }
 
 window.alert('Thanks for playing, bye now!');
-
-/*********** 
-  Functions
- ***********/
-
-// This function generates an integer from min to max
-// copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
-function random(min, max) {
-	const num = Math.floor(Math.random() * (max - min + 1)) + min;
-	return num;
-}
